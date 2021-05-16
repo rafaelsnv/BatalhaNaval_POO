84017ded69ba065699cacaa82cb61be861047e27
Daniel T Nunes <nnsdtr.github@gmail.com>
2021-05-16 18:28:08 -0300

    Consertando arquivo da Classe Tabuleiro

---
+++ Codigos/src/Tabuleiro.java
@@ -0,0 +1,15 @@
+public class Tabuleiro {
+    private static String [ ][ ] GRADE = new String[15][15];
+
+    public Tabuleiro(){
+
+    }
+
+    public boolean inserirEmbarcacoes(Embarcacao qualEmbarcacao) {
+        return true;
+    }
+
+    public boolean inserirAutoEmbarcacoes(Embarcacao qualEmbarcacao) {
+        return true;
+    }
+}
