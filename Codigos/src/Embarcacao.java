56a0081d0db3bc167a7b94ca7c11964e7ab48107
jptdias10 <64169878+jptdias10@users.noreply.github.com>
2021-05-16 21:27:18 -0300

    init

--- Codigos/src/Embarcacao.java
+++ Codigos/src/Embarcacao.java
@@ -1,10 +1,38 @@
 public class Embarcacao {
-    protected String descricao;
-    protected String[][] dimensoes;
-    protected int quantidade;
+    protected String descricao; // 
     protected boolean orientacaoEmbarcacao; // false = horizontal | true = vertical
+    protected ArrayList<Casa> casasOcupadas; // 
+    protected boolean inserido; //
 
     public Embarcacao() {
+        casasOcupadas = new ArrayList<Casa>();
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public void setOrientacaoEmbarcacao(){
+        this.orientacaoEmbarcacao = true;
+    }
+
+    public void setInserido(){
+        this.inserido = true;
+    }
+
+    /** */
+    public boolean inserir(String coluna, int linha){
+        Casa novaCasa = new Casa(coluna, linha);
+        casasOcupadas.add(novaCasa);
+    }
+
+    /** */
+    public boolean atingir(){
+
+    }
+
+    /** */
+    public boolean afundou(){
 
     }
 
