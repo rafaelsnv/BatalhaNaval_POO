21557e3a8c8517e6ce148425fbc404ad9edb7dbc
Gabriel Favero <gabriel.o.favero@live.com>
2021-05-19 09:50:52 -0300

    Primeiras inserções em IJogador
    Por enquanto ainda sem código bruto. Estou analisando a UML e classes desenvolvidas para pensar na implementação. Ainda hoje farei mais modificações.

--- Codigos/src/JogadorPKG/IJogador.java
+++ Codigos/src/JogadorPKG/IJogador.java
@@ -1,4 +1,46 @@
 package JogadorPKG;
 
 public class IJogador {
+
+    public boolean inserirEmbarcacao(Embarcacoes embarcacao, String coluna, int linha) {
+        boolean livre = false;
+
+        //  O código fica aqui
+
+        if (livre) {return true;}
+        else       {return false;}
+
+    }
+
+    public boolean girarAntiHorario(Embarcacoes embarcacao) {
+        boolean livre = false;
+
+        //  O código fica aqui
+
+        if (livre) {return true;}
+        else       {return false;}
+
+    }
+
+    public boolean girarHorario(Embarcacoes embarcacao) {
+        boolean livre = false;
+
+        //  O código fica aqui
+
+        if (livre) {return true;}
+        else       {return false;}
+
+    }
+
+    public boolean bombardear(String coluna, int linha) {
+        boolean livre = false;
+
+        //  O código fica aqui
+
+        if (livre) {return true;}
+        else       {return false;}
+
+    }
+
+
 }
