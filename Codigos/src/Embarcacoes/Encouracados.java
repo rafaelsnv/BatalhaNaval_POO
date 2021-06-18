84f5bb032679eee7788de65e37ebec4cfc6e3584
Daniel T Nunes <nnsdtr.github@gmail.com>
2021-06-18 18:08:17 -0300

    Correção bug Encouraçado

--- Codigos/src/Embarcacoes/Encouracados.java
+++ Codigos/src/Embarcacoes/Encouracados.java
@@ -25,27 +25,22 @@ public class Encouracados extends Embarcacao {
      */
     @Override
     public ArrayList<Casa> setCoordenadas(int linha, int coluna) {
-
-        Casa pedaco = this.embarcacao.get(0); // Primeiro pedaço da embarcação.
-        pedaco.setCoordenadas(linha, coluna); // Insere na primeira casa da embarcação as coordenadas.
-        pedaco.setCor(this.corFonte, this.corFundo);
-        embarcacao.set(0, pedaco); // Insere novas informações de casa na embarcação.
-        pedaco = this.embarcacao.get(1); // Segundo pedaço da embarcação.
-
-        if (!this.orientacaoVertical) // Se embarcação estiver na horizontal.
-            for (int i = 1; i < this.tamanho; i++) { // - Próximas casas recebem
-                pedaco = this.embarcacao.get(i);
-                pedaco.setOcupante(this.ID);
-                pedaco.setCoordenadas(linha, coluna + i); // mesma linha mas colunas diferentes.
+        if (this.orientacaoVertical)                             // Se embarcação estiver na vertical.
+            for (int i = 0; i < this.tamanho; i++) {             // - Próximas casas recebem
+                Casa pedaco = this.embarcacao.get(i);
+                pedaco.setCoordenadas(linha + i, coluna); // mesma linha mas colunas diferentes.
                 pedaco.setCor(this.corFonte, this.corFundo);
+                pedaco.setOcupante(this.ID);
+
                 embarcacao.set(i, pedaco);
             }
         else // Se não.
-            for (int i = 1; i < this.tamanho; i++) { // - Próximas casas recebem
-                pedaco = this.embarcacao.get(i);
-                pedaco.setOcupante(this.ID);
-                pedaco.setCoordenadas(linha + i, coluna); // mesma coluna mas linhas diferentes.
+            for (int i = 0; i < this.tamanho; i++) {             // - Próximas casas recebem
+                Casa pedaco = this.embarcacao.get(i);
+                pedaco.setCoordenadas(linha, coluna + i); // mesma coluna mas linhas diferentes.
                 pedaco.setCor(this.corFonte, this.corFundo);
+                pedaco.setOcupante(this.ID);
+
                 embarcacao.set(i, pedaco);
             }
         return embarcacao;
